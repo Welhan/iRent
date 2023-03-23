4908c581400d5ee86e6e53629d7e0579ddf34f75
Welhan <welhan.susanto@gmail.com>
2023-03-23 11:09:12 +0700

    membuat tampilan menu di template menjadi dinamis sesuai akses view user

--- app/Helpers/fungsi_helper.php
+++ app/Helpers/fungsi_helper.php
@@ -52,3 +52,35 @@ function check_Expired($userID)
         return false;
     }
 }
+
+function generateMenu($user_id)
+{
+    $db = \config\Database::connect();
+
+    $builder = $db->table('user_access_menu');
+    $builder->select('DISTINCT(user_access_menu.menu_id) AS id, mst_menu.menu');
+    $builder->join('mst_menu', 'mst_menu.id = user_access_menu.menu_id');
+    $builder->join('mst_submenu', 'mst_submenu.id = user_access_menu.submenu_id');
+    $builder->where(['user_access_menu.user_id' => $user_id]);
+    $builder->where(['user_access_menu.flag_view' => 1]);
+    $builder->where(['mst_submenu.active' => 1]);
+    $builder->orderBy('mst_menu.id', 'ASC');
+
+    return $builder->get()->getResultObject();
+}
+
+function generateSubmenu($menu_id, $user_id = '')
+{
+    $db = \config\Database::connect();
+
+    $builder = $db->table('mst_submenu');
+    $builder->select('mst_submenu.*');
+    if ($user_id) {
+        $builder->join('user_access_menu', 'mst_submenu.id = user_access_menu.submenu_id');
+        $builder->where(['mst_submenu.menu_id' => $menu_id, 'user_access_menu.flag_view' => 1, 'user_id' => $user_id]);
+    } else {
+        $builder->where('menu_id', $menu_id);
+    }
+
+    return $builder->get()->getResultObject();
+}
